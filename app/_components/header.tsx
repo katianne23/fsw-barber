b71a81984955fe1e145fec0552ace36752f0a769
katianne40@gmail.com <katianne40@gmail.com>
2024-08-06 15:04:55 -0300

    feat: add Haeader componentes and Banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+
+const Header = () => {
+    return ( 
+        <Card>
+            <CardContent className="p-5 flex flex-row  items-center justify-between">
+                <Image src="/logo.png" height={18} width={120} alt="FSW Barber" />
+                <Button size="icon" variant="outline">
+                    <MenuIcon />
+                </Button>
+            </CardContent>
+        </Card>
+     );
+}
+ 
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
+import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Home Page!!</h1>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p>Terça-feira, 05 de agosto.</p>
+
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full  h-[150px] mt-6 rounded-xl">
+          <Image alt="Agende nos melhores  com FSW Barber" src="/banner-01.png" fill className="object-cover rounded-xl" />
+        </div>
+      </div>
+    </div>
+  )
 }
