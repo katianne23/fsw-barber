2c92585719be1a3fc60b677ae7a080bd0c462d4a
katianne40@gmail.com <katianne40@gmail.com>
2024-08-08 17:16:43 -0300

    feat: add initial barbershop page

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
 import { StarIcon } from "lucide-react";
+import Link from "next/link";
 
 interface BarbershopItemProps {
     barbershop: Barbershop
@@ -14,22 +15,24 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
         <Card className="min-w-[167px] rounded-2xl">
             <CardContent className="p-0 px-1 pt-1 pb-2">
                 <div className="relative h-[159px] w-full">
-                    <Image 
-                        fill 
-                        className="object-cover rounded-2xl" 
-                        src={barbershop.imageUrl} 
-                        alt={barbershop.name} 
+                    <Image
+                        fill
+                        className="object-cover rounded-2xl"
+                        src={barbershop.imageUrl}
+                        alt={barbershop.name}
                     />
                     <Badge className="absolute left-2 top-2 space-x-1" variant="secondary">
-                        <StarIcon size={12} className="fill-primary text-primary"/>
+                        <StarIcon size={12} className="fill-primary text-primary" />
                         <p className="text-xs font-semibold">5,0</p>
                     </Badge>
-                
+
                 </div>
                 <div className="py-3 px-1">
                     <h3 className="truncate font-semibold">{barbershop.name}</h3>
                     <p className="text-sm text-gray-400 truncate">{barbershop.address}</p>
-                    <Button variant="secondary" className="mt-3 w-full">Reservar</Button>
+                    <Button variant="secondary" className="mt-3 w-full" asChild>
+                        <Link href={`/barbershops/${barbershop.id}`}>Reservar</Link>
+                    </Button>
                 </div>
 
             </CardContent>
