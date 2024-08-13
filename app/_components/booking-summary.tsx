0a73cb66a95addf65f223031b1f95a2e623d3058
katianne40@gmail.com <katianne40@gmail.com>
2024-08-13 11:26:32 -0300

    feat: ensure booking details can be viewed

---
+++ app/_components/booking-summary.tsx
@@ -0,0 +1,14 @@
+// import { ptBR } from "date-fns/locale";
+// import { Card, CardContent } from "./ui/card";
+// import { format } from "date-fns";
+
+// interface BookingSummaryProps {
+
+// }
+// const BookingSummary = () => {
+//     return ( 
+       
+//      );
+// }
+ 
+// export default BookingSummary;
